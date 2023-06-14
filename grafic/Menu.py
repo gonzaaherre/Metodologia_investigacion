ee64871e3ebb8b2e7c2575647232698c831a0fc1
Gonzalo Herrera <gonzaaherre@gmail.com>
2023-06-14 11:33:17 -0300

    creacion de ruleta

--- grafic/Menu.py
+++ grafic/Menu.py
@@ -1,25 +1,70 @@
+import tkinter as tk
 from tkinter import *
+from math import *
+
+import random
 class Opciones(Toplevel):
     def __init__(self,master=None):
         super().__init__(master=master)
         self.title("Opciones")
         self.geometry("300x300")
-        label = Label(self,text="No implementado")
+        label = Label(self,text="lazaro puto")
         label.pack()
         self.grab_set()
 class Ruleta(Toplevel):
-    def __init__(self,master=None):
+    def __init__(self, master=None):
         super().__init__(master=master)
         self.title("Ruleta")
         self.geometry("500x600")
-        label_ruleta=Label(self,text="No implementado")
-        label_ruleta.pack()
+        canvas = Canvas(self, width=400, height=400)
+        canvas.pack()
+        self.dibujar_ruleta(canvas)
         self.grab_set()
+
+    def dibujar_ruleta(self, canvas):
+        # Configuración de colores
+        color_fondo = "#FFFFFF"  # Color de fondo de la ruleta
+        color_lineas = "#000000"  # Color de las líneas
+        color_numeros = "#FF0000"  # Color de los números
+
+        # Dibujar fondo de la ruleta
+        # canvas.create_oval(50, 50, 350, 350, fill=color_fondo, outline=color_lineas, width=2)
+
+        # Dibujar líneas
+        for i in range(0, 360, 18):
+            angulo = i * 3.14159 / 180.0
+            x1 = 200 + 140 * sin(angulo)
+            y1 = 200 - 140 * cos(angulo)
+            x2 = 200 + 160 * sin(angulo)
+            y2 = 200 - 160 * cos(angulo)
+            canvas.create_line(x1, y1, x2, y2, fill=color_lineas, width=2)
+
+        # Dibujar números
+        for i in range(0, 360, 18):
+            angulo = i * 3.14159 / 180.0
+            x = 200 + 130 * sin(angulo)
+            y = 200 - 130 * cos(angulo)
+            canvas.create_text(x, y, text=str(i), fill=color_numeros, font=("Arial", 12), anchor=NW)
+
+
 menu = Tk()
 menu.geometry('800x533')
 menu.title('Menu principal')
 menu.resizable(width=False, height=False)
-bg = PhotoImage(file="images/fondo.png")
+
+# Configuración de la imagen de fondo
+bg_image = PhotoImage(file="")  # Reemplaza "ruta_de_la_imagen.png" con la ruta real de tu imagen
+label1 = Label(menu, image=bg_image)
+label1.place(x=0, y=0)
+
+titulo = Label(menu, text='Bienvenido al Casino')
+titulo.pack(pady=10)
+        
+menu = Tk()
+menu.geometry('800x533')
+menu.title('Menu principal')
+menu.resizable(width=False, height=False)
+bg = PhotoImage(file="")
 label1 = Label(menu, image=bg)
 label1.place(x=0,y=0)
 titulo = Label(menu, text='Bienvenido al Casino')
